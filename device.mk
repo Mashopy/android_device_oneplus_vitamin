5daf7bd704fe87d4cb33ff85604ea0e8cbfad78c
Mashopy <eliasgheeraert@gmail.com>
2023-08-24 22:06:38 -0400

    vitamin: Build AOSP media related blobs

--- device.mk
+++ device.mk
@@ -196,6 +196,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
 # Media
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.2.vendor \
+    libavservices_minijail_vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml
 
