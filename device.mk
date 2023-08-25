d2de86b6d39b1ae3f6aee88e454cf53641f422b7
Mashopy <eliasgheeraert@gmail.com>
2023-08-24 22:01:22 -0400

    vitamin: Build OSS MediaTek v2 USB 1.3 service

--- device.mk
+++ device.mk
@@ -304,6 +304,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl
 
 # USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-mediatekv2
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
